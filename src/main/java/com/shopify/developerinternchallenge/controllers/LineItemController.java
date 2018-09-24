40d968ee0aef64f03343df9fabf4a3dc96f1e6e3
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-09-24 03:59:35 -0300

    Add LineItem API with GET, GET ALL, POST, DELETE and PATCH operations

--- src/main/java/com/shopify/developerinternchallenge/controllers/LineItemController.java
+++ src/main/java/com/shopify/developerinternchallenge/controllers/LineItemController.java
@@ -5,14 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shopify.developerinternchallenge.models.exceptions.NotFoundException;
 import com.shopify.developerinternchallenge.models.lineitems.LineItem;
 import com.shopify.developerinternchallenge.models.order.Order;
 import com.shopify.developerinternchallenge.models.product.Product;
+import com.shopify.developerinternchallenge.services.LineItemService;
+import com.shopify.developerinternchallenge.services.OrderService;
 
 @RestController
 @CrossOrigin("*")
@@ -23,6 +27,10 @@ public class LineItemController {
 	ProductController productController;
 	@Autowired
 	OrderController orderController;
+	@Autowired
+	OrderService orderService;
+	@Autowired
+	LineItemService lineItemService;
 
 	@RequestMapping(value = ShopController.ENDPOINT + "/{shopName}" + ProductController.ENDPOINT + "/{productId}"
 			+ LineItemController.ENDPOINT, method = RequestMethod.GET)
@@ -37,7 +45,7 @@ public class LineItemController {
 	public @ResponseBody LineItem getProductLineItem(@PathVariable String shopName, @PathVariable Long productId,
 			@PathVariable Long lineItemId) {
 		Product product = this.productController.getProduct(shopName, productId);
-		return product.getLineItemWithId(lineItemId);
+		return getLineItemById(lineItemId, product);
 	}
 
 	@RequestMapping(value = ShopController.ENDPOINT + "/{shopName}" + OrderController.ENDPOINT + "/{orderId}"
@@ -52,6 +60,50 @@ public class LineItemController {
 	public @ResponseBody LineItem getOrderLineItem(@PathVariable String shopName, @PathVariable Long orderId,
 			@PathVariable Long lineItemId) {
 		Order order = this.orderController.getShopOrder(shopName, orderId);
-		return order.getLineItemWithId(lineItemId);
+		return getLineItemById(lineItemId, order);
 	}
+
+	@RequestMapping(value = ShopController.ENDPOINT + "/{shopName}" + OrderController.ENDPOINT + "/{orderId}"
+			+ LineItemController.ENDPOINT, method = RequestMethod.POST)
+	public @ResponseBody LineItem addOrderLineItem(@PathVariable String shopName, @PathVariable Long orderId,
+			@RequestBody LineItem lineItem) {
+		Order order = this.orderController.getShopOrder(shopName, orderId);
+		return this.orderService.addLineItem2Order(lineItem, order);
+	}
+
+	@RequestMapping(value = ShopController.ENDPOINT + "/{shopName}" + OrderController.ENDPOINT + "/{orderId}"
+			+ LineItemController.ENDPOINT + "/{lineItemId}", method = RequestMethod.DELETE)
+	public @ResponseBody Boolean deleteOrderLineItem(@PathVariable String shopName, @PathVariable Long orderId,
+			@PathVariable Long lineItemId) {
+		Order order = this.orderController.getShopOrder(shopName, orderId);
+		LineItem lineItem = getLineItemById(lineItemId, order);
+		this.orderService.deleteLineItemFromOrder(lineItem, order);
+		return true;
+	}
+
+	@RequestMapping(value = ShopController.ENDPOINT + "/{shopName}" + OrderController.ENDPOINT + "/{orderId}"
+			+ LineItemController.ENDPOINT + "/{lineItemId}/amount/{newAmount}", method = RequestMethod.PATCH)
+	public @ResponseBody LineItem editOrderLineItemProductAmount(@PathVariable String shopName,
+			@PathVariable Long orderId, @PathVariable Long lineItemId, @PathVariable Integer newAmount) {
+		Order order = this.orderController.getShopOrder(shopName, orderId);
+		LineItem lineItem = getLineItemById(lineItemId, order);
+		return this.lineItemService.editLineItemAmount(lineItem, newAmount);
+	}
+
+	private LineItem getLineItemById(Long lineItemId, Order order) {
+		LineItem lineItem = order.getLineItemWithId(lineItemId);
+		if (lineItem == null) {
+			throw new NotFoundException(lineItemId.toString());
+		}
+		return lineItem;
+	}
+
+	private LineItem getLineItemById(Long lineItemId, Product product) {
+		LineItem lineItem = product.getLineItemWithId(lineItemId);
+		if (lineItem == null) {
+			throw new NotFoundException(lineItemId.toString());
+		}
+		return lineItem;
+	}
+
 }
